ae4bd448e226f7e0014d2d294cccecbcc79e841d
Emily Jennings <emily.jennings@flatironschool.com>
2018-05-23 17:39:41 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,7 @@
+board = get_variable_from_file("./bin/move", "board")
+
 def display_board(board)
+
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -6,4 +9,14 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+
+
+def input_to_index(input)
+  i = input.to_i
+  i = i-1
+end
+
+
+def move(array, index, value="X")
+  array[index]=value
+end
